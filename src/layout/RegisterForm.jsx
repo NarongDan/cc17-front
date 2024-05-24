e75e6a090a2710bd8b590ac401937d1e9c5b5677
NarongDan <narong.dan1@gmail.com>
2024-05-24 15:02:36 +0700

    registerform

---
+++ src/layout/RegisterForm.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function RegisterForm() {
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      // validation
+
+      if (input.password !== input.confirmPassword) {
+        return alert("Check confirm password");
+      }
+      // จัด input ส่ง api
+      const rs = await axios.post("http://localhost:8889/auth/register", input);
+      console.log(rs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-4xl my-10 text-center">Register Form</h1>
+      <form
+        className="border w-3/4 mx-auto p-4 rounded-lg"
+        onSubmit={handleSubmit}
+      >
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Username:</span>
+          </div>
+          <input
+            name="username"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full "
+            value={input.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Password:</span>
+          </div>
+          <input
+            name="password"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full "
+            value={input.password}
+            onChange={handleChange}
+          />
+        </label>
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Confirm Password:</span>
+          </div>
+          <input
+            name="confirmPassword"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full "
+            value={input.confirmPassword}
+            onChange={handleChange}
+          />
+        </label>
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Email:</span>
+          </div>
+          <input
+            name="email"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full "
+            value={input.email}
+            onChange={handleChange}
+          />
+        </label>
+        <button className="btn btn-outline btn-primary mt-5 w-full">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,30 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import Header from "../layout/Header";
+import LoginForm from "../layout/LoginForm";
+import RegisterForm from "../layout/RegisterForm";
+
+const guestRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Outlet />
+      </>
+    ),
+    errorElement: <Navigate to="/" />,
+    children: [
+      { index: true, element: <LoginForm /> },
+      { path: "/register", element: <RegisterForm /> },
+    ],
+  },
+]);
+
+export default function AppRouter() {
+  return <RouterProvider router={guestRouter} />;
+}
